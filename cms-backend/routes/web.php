a2c16fce2b76132747fc5fde737dffa2d9c0f762
irebuv <irebuv671@gmail.com>
2025-10-03 19:09:06 +0300

    setup tailwind and vite

--- cms-backend/routes/web.php
+++ cms-backend/routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Access-Control-Allow-Origin: http://localhost:5173');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
     exit(0);
 }
 
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: http://localhost:5173');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
@@ -17,15 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-    Route::get('/test', function () {
-        return response()->json([
-            'message' => 'API работает в Laravel 12!',
-            'status' => 'success',
-            'timestamp' => now()
-        ]);
-    });
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'API работает в Laravel 12!',
+        'status' => 'success',
+        'timestamp' => now()
+    ]);
+});
 
-    Route::get('/posts', function () {
-        $users = \Illuminate\Support\Facades\DB::table('users')->get();
-        return response()->json($users);
-    });
+Route::get('/posts', function () {
+    $users = \Illuminate\Support\Facades\DB::table('users')->get();
+    return response()->json($users);
+});
