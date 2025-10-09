42049625712999cfe6c88254bc8b9636147314a7
irebuv <irebuv671@gmail.com>
2025-10-09 16:33:50 +0300

    rename backend folder

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -29,7 +28,6 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        Auth::shouldUse('api');
         $credentials = $request->only('email', 'password');
         if (!$token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Invalid credentials'], 401);
